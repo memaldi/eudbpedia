a18ded524c2faafac7b07e70712f29e856501d03
Mikel Emaldi <m.emaldi@gmail.com>
2020-02-13 15:02:15 +0100

    Preparing the buildconfig for dbpedia pod

--- extractor/entrypoint.sh
+++ extractor/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+export MAVEN_OPTS="-Duser.home=/dbpedia/extraction-framework"
 cd /dbpedia/extraction-framework
+mvn clean install -DskipTests
 cd dump
 ../run download /dbpedia/config/download.eu.properties
 ../run extraction /dbpedia/config/extraction.eu.properties
+cp /dbpedia/dump/*.ttl.bz2 /dbpedia/import/
